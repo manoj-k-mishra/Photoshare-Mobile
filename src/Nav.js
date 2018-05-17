d4c48dcb5666562cbf221ca564f034259c72375c
manoj-k-mishra <manojmishra571980@gmail.com>
2018-05-17 19:50:41 +1000

    two tabs working-explore and feedscreen

--- src/Nav.js
+++ src/Nav.js
@@ -2,25 +2,13 @@ import { Navigation } from 'react-native-navigation';
 import { registerScreens } from './screens';
 
 registerScreens();
-export default class Nav{
-    constructor(){  this._initApp()    }
-    _initApp(){
-        Navigation.isAppLaunched()
-      .then((appLaunched) => {
-        if (appLaunched) {
-          startApp();
-        }
-        new NativeEventsReceiver().appLaunched(startApp);
-      }),
-        Navigation.startTabBasedApp({
-            tabs:[
-                {
-                    label: 'Feeds',
-                    screen: 'instagram_mobile.FeedsScreen',
-                    title: 'Instagram',
-                    icon: 'aa',
-                }
-            ]
-        })
-    }
+export default class Nav
+{    constructor(){  this._initApp()    }
+     _initApp()
+       {    Navigation.startTabBasedApp(
+           { tabs:[  {  label: 'Feeds',  screen: 'instagram_mobile.FeedsScreen', title: 'Instagram', icon: 'icon1', },
+                     { label: 'Explore', screen: 'instagram_mobile.ExploreScreen', title: 'Explore', icon: 'icon2', },
+                  ]
+           })
+       }
 }
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,7 +1,8 @@
 import { Navigation } from 'react-native-navigation';
 import FeedsScreen from './FeedsScreen';
+import ExploreScreen from './ExploreScreen'
 
 export const registerScreens = () => {
     Navigation.registerComponent('instagram_mobile.FeedsScreen', () => FeedsScreen)
-  
+    Navigation.registerComponent('instagram_mobile.ExploreScreen', () => ExploreScreen)
 }
