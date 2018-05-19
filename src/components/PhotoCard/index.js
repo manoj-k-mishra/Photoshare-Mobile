b05a7802ae7573da3205996bee05fdddc8dd0153
manoj-k-mishra <manojmishra571980@gmail.com>
2018-05-19 12:44:03 +1000

    PhotoCard component created and inserted in feedscreen

---
+++ src/components/PhotoCard/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+class PhotoCard extends Component 
+{   state = {};
+    render() { return (
+                        <View >
+                        <Text>Hello PhotoCard</Text>
+                        </View>
+                    );
+            }
+  }
+  
+  export default PhotoCard;
--- src/screens/FeedsScreen/index.js
+++ src/screens/FeedsScreen/index.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { PhotoCard } from '../../components'
 
-class FeedsScreen extends Component {
-    state={}
-    render(){
-        return(
-            <View>
-                <Text>FeedScreen</Text>
-            </View>
-        );
-    }
+class FeedsScreen extends Component 
+{  state={}
+    render(){  return(
+                        <View>
+                            <PhotoCard/>
+                            <Text>FeedScreen</Text>
+                        </View>
+                    );
+            }
 
 }
 
