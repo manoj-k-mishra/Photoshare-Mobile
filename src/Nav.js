c647b88a1306c8fe664d95d0b5456428cf516277
manojkmishra <manojkm1980@gmail.com>
2018-05-19 12:16:39 +1000

    react-native-vector-icons working

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,16 @@
 import { Navigation } from 'react-native-navigation';
 import { registerScreens } from './screens';
+import { iconsLoaded, iconsMap } from './utils/themes';
 
 registerScreens();
 export default class Nav
-{    constructor(){  this._initApp()    }
+{    constructor(){  //this._initApp()  
+                     iconsLoaded.then(() => this._initApp());  
+                  }
      _initApp()
        {    Navigation.startTabBasedApp(
-           { tabs:[  {  label: 'Feeds',  screen: 'instagram_mobile.FeedsScreen', title: 'Instagram', icon: 'icon1', },
-                     { label: 'Explore', screen: 'instagram_mobile.ExploreScreen', title: 'Explore', icon: 'icon2', },
+           { tabs:[  {  label: 'Feeds',  screen: 'instagram_mobile.FeedsScreen', title: 'Instagram',   icon: iconsMap.home },
+                     { label: 'Explore', screen: 'instagram_mobile.ExploreScreen', title: 'Explore',  icon: iconsMap['ios-search'] },
                   ]
            })
        }
