d7fc76b47ec184c233c49929603510f8b7d30afa
manoj-k-mishra <manojmishra571980@gmail.com>
2018-05-19 15:28:13 +1000

    header style change

--- src/components/PhotoCard/Header.js
+++ src/components/PhotoCard/Header.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { View, Text, StyleSheet, } from 'react-native';
 
 const styles = StyleSheet.create(
-{  root: {  height: 60,  backgroundColor: 'magenta' },
+{  root: {  height: 60,  backgroundColor: 'white' , flexDirection: 'row', paddingHorizontal: 16,},
+   userMetaWrapper: { flex: 1, backgroundColor:'indigo',  }, 
+   btnWrapper: { flex: 0.1, justifyContent: 'center', alignItems: 'center', },
 });
 
 export default function Header()
 {    return( <View style={styles.root}>
-                 <Text>Header</Text>
+                <View style={styles.userMetaWrapper}></View>
+                <View style={styles.btnWrapper}></View>
+              
+                
               </View>
             );
 }
